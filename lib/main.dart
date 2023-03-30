3a43207f4cfbfe6caa3997c93a02f9fd80f1c798
kouahou ivan <ivankouamou21@gmail.com>
2023-03-30 16:08:16 +0100

    push_final_2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:scan37/components/bottomNavbar.dart';
+
+import 'components/bottomNavbar.dart';
 
 void main() {
   runApp(const MyApp());
